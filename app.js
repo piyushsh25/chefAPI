1f583efb7732f2927f281d27700a9b26fc599cf2
piyushpoudyal <72690317+piyushsh25@users.noreply.github.com>
2022-01-14 15:45:49 +0530

    refractor: es5 to es6

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
-var textTranslate=document.querySelector("#text-area");
-var btnTranslate=document.querySelector("#btn-translate");
-var textOutput=document.querySelector("#txt-output");
+const textTranslate=document.querySelector("#text-area");
+const btnTranslate=document.querySelector("#btn-translate");
+const textOutput=document.querySelector("#txt-output");
 
-var serverURL="	https://api.funtranslations.com/translate/chef.json";
+let serverURL="	https://api.funtranslations.com/translate/chef.json";
 
-function getTranslationURL(text){
+const getTranslationURL=(text)=>{
     return serverURL+"?"+"text="+text;
 }
 
 btnTranslate.addEventListener("click",function clickEventHandler(){
-    var text=textTranslate.value;
+    let text=textTranslate.value;
 fetch(getTranslationURL(text))
 .then(response=>response.json())
 .then(json=>{
-    var translatedText=json.contents.translated;
+    let translatedText=json.contents.translated;
     textOutput.innerText=translatedText;
     console.log(json.contents.translated);
 })
 .catch(console.log("error occured"+Error))
 })
-
-
-
